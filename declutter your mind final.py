58607a21f169ea7a289fae8aa3602805b78e782e
SABG4 <sirba.a.b.gabbert@stud.leuphana.de>
2026-09-02 18:19:13 +0200

    changes

--- declutter your mind final.py
+++ declutter your mind final.py
@@ -37,8 +37,8 @@ def play():
             PLAY_ROOM = pygame.transform.smoothscale(PLAY_ROOM, (860, 560))
             SCREEN.blit(PLAY_ROOM, (40, 40))
 
-            PLAY_BACK = Button(image=None, pos=(640, 460),
-                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="#82cf88")
+            PLAY_BACK = Button(image=None, pos=(77, 22),
+                           text_input="BACK", font=get_font(65), base_color="White", hovering_color="#82cf88")
 
             PLAY_BACK.changeColor(PLAY_MOUSE_POS)
             PLAY_BACK.update(SCREEN)
@@ -55,6 +55,33 @@ def play():
 
 
 def info():
+    pygame.display.set_caption('what is happening')
+    while True:
+        INFO_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("lightsalmon3")
+
+        INFO_SCREEN = pygame.image.load("media/infopage.jpg").convert_alpha()
+        INFO_SCREEN = pygame.transform.smoothscale(INFO_SCREEN, (860, 560))
+        SCREEN.blit(INFO_SCREEN, (40, 40))
+
+        INFO_BACK = Button(image=None, pos=(640, 460),
+                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="#82cf88")
+
+        INFO_BACK.changeColor(INFO_MOUSE_POS)
+        INFO_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if INFO_BACK.checkForInput(INFO_MOUSE_POS):
+                    return()
+
+        pygame.display.update()
+
+'''def set_the_mood():
     pygame.display.set_caption('what is happening')
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
@@ -80,6 +107,7 @@ def info():
                     start_screen()
 
         pygame.display.update()
+'''
 
 # main menu/ start screen used from github: baraltech and adapted for my specific cases
 def start_screen():
@@ -92,11 +120,11 @@ def start_screen():
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
 
-        PLAY_BUTTON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/play button.png"),(135,176)), pos=(700, 500),
+        PLAY_BUTTON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/play button.png"),(75,86)), pos=(700, 500),
                              text_input="PLAY", font=get_font(25), base_color="lightsalmon3", hovering_color="#82cf88")
 
-        INFO_BUTTON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/info button.png"), (200, 241)),
-                             pos=(580, 510),
+        INFO_BUTTON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/info button.png"), (40, 86)),
+                             pos=(580, 500),
                              text_input="INFO", font=get_font(25), base_color="lightsalmon3", hovering_color="#82cf88")
 
         for button in [PLAY_BUTTON]:
@@ -114,7 +142,9 @@ def start_screen():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
-                    sys.exit()
+                if INFO_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        info()
+
 
         pygame.display.update()
 
