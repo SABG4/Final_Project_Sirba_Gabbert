30296ca1b82647881ec960b4ebf777f72dfffe9b
SABG4 <sirba.a.b.gabbert@stud.leuphana.de>
2026-09-06 18:01:49 +0200

    start screen with new buttons

--- declutter your mind final.py
+++ declutter your mind final.py
@@ -14,10 +14,6 @@ SCREEN.fill('lightsalmon3')
 # set the pygame window name
 pygame.display.set_caption('✨declutter your mind✨')
 
-#including sound into my game
-pygame.mixer.music.load('media/music.mp3')
-pygame.mixer.music.play(-1)
-
 # create a surface object, image is drawn on it.
 imp = pygame.image.load("media/start screen.jpg").convert_alpha()
 imp = pygame.transform.smoothscale(imp, (860, 560))
@@ -31,14 +27,14 @@ def play():
         while True:
             PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-            SCREEN.fill("lightsalmon3")
+            SCREEN.fill("aliceblue")
 
             PLAY_ROOM = pygame.image.load("media/room.jpg").convert_alpha()
             PLAY_ROOM = pygame.transform.smoothscale(PLAY_ROOM, (860, 560))
             SCREEN.blit(PLAY_ROOM, (40, 40))
 
-            PLAY_BACK = Button(image=None, pos=(77, 22),
-                           text_input="BACK", font=get_font(65), base_color="White", hovering_color="#82cf88")
+            PLAY_BACK = Button(image=None, pos=(83, 22),
+                           text_input="BACK", font=get_font(45), base_color="#a2a3bb", hovering_color="#b2d6b5")
 
             PLAY_BACK.changeColor(PLAY_MOUSE_POS)
             PLAY_BACK.update(SCREEN)
@@ -66,7 +62,7 @@ def info():
         SCREEN.blit(INFO_SCREEN, (40, 40))
 
         INFO_BACK = Button(image=None, pos=(640, 460),
-                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="#82cf88")
+                           text_input="BACK", font=get_font(45), base_color="White", hovering_color="#b2d6b5")
 
         INFO_BACK.changeColor(INFO_MOUSE_POS)
         INFO_BACK.update(SCREEN)
@@ -81,33 +77,43 @@ def info():
 
         pygame.display.update()
 
-'''def set_the_mood():
-    pygame.display.set_caption('what is happening')
+def settings():
+    pygame.display.set_caption('settings')
+
+    '''# including sound into my game
+    pygame.mixer.music.load('media/music.mp3')
+    pygame.mixer.music.play(-1)
+
+
+    MUSIC_ON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/music on.png"), (40, 86)),
+                             pos=(580, 500),
+                             text_input="ON", font=get_font(25), base_color="lightsalmon3", hovering_color="#b2d6b5")
+          '''
     while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+        SETTINGS_MOUSE_POS = pygame.mouse.get_pos()
 
-        screen.fill("lightsalmon3")
+        SCREEN.fill("lightsalmon3")
 
-        INFO_SCREEN = pygame.image.load("media/room.jpg").convert_alpha()
-        INFO_SCREEN = pygame.transform.smoothscale(INFO_SCREEN, (860, 560))
-        screen.blit(INFO_SCREEN, (40, 40))
+        SETTINGS_SCREEN = pygame.image.load("media/settingspage.jpeg").convert_alpha()
+        SETTINGS_SCREEN = pygame.transform.smoothscale(SETTINGS_SCREEN, (860, 560))
+        SCREEN.blit(SETTINGS_SCREEN, (40, 40))
 
-        INFO_BACK = Button(image=None, pos=(640, 460),
-                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="#82cf88")
+        SETTINGS_BACK = Button(image=None, pos=(640, 460),
+                           text_input="BACK", font=get_font(45), base_color="White", hovering_color="#b2d6b5")
 
-        INFO_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
+        SETTINGS_BACK.changeColor(SETTINGS_MOUSE_POS)
+        SETTINGS_BACK.update(SCREEN)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    start_screen()
+                if SETTINGS_BACK.checkForInput(SETTINGS_MOUSE_POS):
+                    return ()
 
         pygame.display.update()
-'''
+
 
 # main menu/ start screen used from github: baraltech and adapted for my specific cases
 def start_screen():
@@ -120,18 +126,26 @@ def start_screen():
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
 
-        PLAY_BUTTON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/play button.png"),(75,86)), pos=(700, 500),
-                             text_input="PLAY", font=get_font(25), base_color="lightsalmon3", hovering_color="#82cf88")
+        PLAY_BUTTON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/play button.png"),(85,96)), pos=(580, 500),
+                             text_input="PLAY", font=get_font(25), base_color="lightsalmon3", hovering_color="#b2d6b5")
 
         INFO_BUTTON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/info button.png"), (40, 86)),
-                             pos=(580, 500),
-                             text_input="INFO", font=get_font(25), base_color="lightsalmon3", hovering_color="#82cf88")
+                             pos=(460, 500),
+                             text_input="INFO", font=get_font(25), base_color="lightsalmon3", hovering_color="#b2d6b5")
+
+        SETTINGS_BUTTON = Button(image=pygame.transform.smoothscale(pygame.image.load("media/settings button.png"), (79, 86)),
+                             pos=(700, 500),
+                             text_input="SETTINGS", font=get_font(25), base_color="lightsalmon3", hovering_color="#b2d6b5")
 
         for button in [PLAY_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
 
         for button in [INFO_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for button in [SETTINGS_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
 
@@ -143,7 +157,9 @@ def start_screen():
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
                 if INFO_BUTTON.checkForInput(MENU_MOUSE_POS):
-                        info()
+                    info()
+                if SETTINGS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    settings()
 
 
         pygame.display.update()
